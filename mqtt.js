87613d9747723e5fb9cb9fa7a7c2d57098ed6c26
vj-wizard-of-code <vedj5830@gmail.com>
2026-03-31 20:20:26 +0530

    added authentication to mqtt

--- mqtt.js
+++ mqtt.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("./user");
+
 const mqtt = require("mqtt")
 const { Vehicle } = require("./vehicle");
 
@@ -10,40 +13,66 @@ client.on("connect", () => {
 
 // Receive + Save
 client.on("message", async (topic, message) => {
+  try {
+    const data = JSON.parse(message.toString());
 
-    try {
-        const data = JSON.parse(message.toString());
-
-        if (
-            !data.vehicle_id ||
-            typeof data.lat !== "number" ||
-            typeof data.lon !== "number" ||
-            !data.timestamp
-        ) {
-            console.log("Invalid data ignored");
-            return;
-        }
-
-        const existing = await Vehicle.findOne({ vehicle_id: data.vehicle_id });
-
-        // Ignore old data
-        if (existing && existing.timestamp > data.timestamp) {
-            console.log("Ignored old data");
-            return;
-        }
-
-        await Vehicle.findOneAndUpdate(
-            { vehicle_id: data.vehicle_id },
-            {
-                ...data,
-                lastUpdated: new Date()
-            },
-            { upsert: true }
-        );
-
-        console.log("Saved to DB:", data);
-
-    } catch (err) {
-        console.log("JSON ERROR:", err.message);
+    // Basic validation
+    if (
+      !data.vehicle_id ||
+      typeof data.lat !== "number" ||
+      typeof data.lon !== "number" ||
+      !data.timestamp ||
+      !data.token
+    ) {
+      console.log("Invalid data");
+      return;
     }
+
+    //VERIFY TOKEN
+    let decoded;
+    try {
+      decoded = jwt.verify(data.token, process.env.JWT_SECRET);
+    } catch {
+      console.log("Invalid JWT");
+      return;
+    }
+
+    //CHECK USER IS DRIVER
+    if (decoded.role !== "driver") {
+      console.log("Not a driver");
+      return;
+    }
+
+    //verify driver owns this vehicle
+    const user = await User.findById(decoded.userId);
+
+    if (!user || user.vehicle_id !== data.vehicle_id) {
+      console.log("Vehicle mismatch");
+      return;
+    }
+
+    // Ignore old data
+    const existing = await Vehicle.findOne({ vehicle_id: data.vehicle_id });
+
+    if (existing && existing.timestamp > data.timestamp) {
+      return;
+    }
+
+    // Save data
+    const {token, ...safeData} = data; // removing token before saving to DB
+
+    await Vehicle.findOneAndUpdate(
+      { vehicle_id: data.vehicle_id },
+      {
+        ...safeData,
+        lastUpdated: new Date()
+      },
+      { upsert: true }
+    );
+
+    console.log("Saved:", data.vehicle_id);
+
+  } catch (err) {
+    console.log("MQTT ERROR:", err.message);
+  }
 });
